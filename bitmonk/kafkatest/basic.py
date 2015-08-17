46c9cbf04f312dae96e7bf0f17ce3b2acd6861a4
Justin Alan Ryan <juryan@ziprealty.com>
2015-08-17 18:04:20 -0400

    86400 loop, tested.

--- bitmonk/kafkatest/basic.py
+++ bitmonk/kafkatest/basic.py
@@ -1,4 +1,4 @@
-import logging
+import logging,time
 from pykafka import KafkaClient
 
 logger = logging.getLogger('pykafka.cluster')
@@ -7,4 +7,10 @@ ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
 client = KafkaClient(hosts='10.100.6.241:9092,10.100.6.242:9092,10.100.6.243:9092')
+topic = client.topics['bitmonk2']
+p = topic.get_producer()
+
+for i in range(86400):
+  p.produce(["message %s" % str(i)])
+  time.sleep(1)
 
