a3e4d3ad35f860b3449b5c29cd72728615b32e12
Justin Ryan <juryan@ziprealty.com>
2015-08-19 12:39:38 -0700

    loop forever, log a time, exit on keyboard interrupt.

--- bitmonk/kafkatest/basic.py
+++ bitmonk/kafkatest/basic.py
@@ -10,7 +10,10 @@ client = KafkaClient(hosts='10.100.6.241:9092,10.100.6.242:9092,10.100.6.243:909
 topic = client.topics['bitmonk2']
 p = topic.get_producer()
 
-for i in range(86400):
-  p.produce(["message %s" % str(i)])
-  time.sleep(0.01)
+while time.sleep(0.01):  
+  try:
+    p.produce(["message sent at  %s" % time.time()])
+  except KeyboardInterrupt:
+    import sys
+    sys.exit()
 
